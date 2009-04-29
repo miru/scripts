fd2fc3270fa8477fc033d4e5e898906a6c7dbe2f
miru <mirupreza@gmail.com>
2009-04-29 11:35:19 +0900

    modified:   sqlite-vacuum.sh

--- sqlite-vacuum.sh
+++ sqlite-vacuum.sh
@@ -1,6 +1,6 @@
 #!/opt/local/bin/zsh
 for FIL in `find . -name "*.sqlite"`
 do
-ls -al $FIL;sqlite3 $FIL "vacuum";ls -la $FIL
+ls -al $FIL; sqlite3 $FIL "vacuum"; sqlite3 $FIL "reindex" ls -la $FIL
 done
 
