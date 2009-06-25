220509550fb5c5065639edea559a98f9def4b2df
miru <mirupreza@gmail.com>
2009-06-25 09:03:54 +0900

    new file:   ldrize_cooperation.js 	modified:   sqlite-vacuum.sh

--- sqlite-vacuum.sh
+++ sqlite-vacuum.sh
@@ -1,6 +1,10 @@
 #!/opt/local/bin/zsh
 for FIL in `find . -name "*.sqlite"`
 do
-ls -al $FIL; sqlite3 $FIL "vacuum"; sqlite3 $FIL "reindex" ls -la $FIL
+ls -al $FIL
+sqlite3 $FIL "vacuum"
+sqlite3 $FIL "reindex"
+ls -la $FIL
+echo ""
 done
 
